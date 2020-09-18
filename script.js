8237f1da121eae271427e9c4444d756c15a9acef
AlxHuff <alexandra.a.huffman@gmail.com>
2020-09-18 14:11:38 -0400

    connected weather API to html elements

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 /// DEFINED FUNCTIONS ///
 
 /// START CONNECTION TO API ///
-
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#weather-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.name;
+  let liDescription = document.querySelector("#description");
+  liDescription.innerHTML = response.data.weather[0].description;
+  let liHumid = document.querySelector("#humid");
+  liHumid.innerHTML = response.data.main.humidity;
+  let liWind = document.querySelector("#wind");
+  liWind.innerHTML = Math.round(response.data.wind.speed);
+  let iconElement = document.querySelector("#main-icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
+}
+let apiKey = "36616c1d79a2b7725ac3053f800c78b3";
+let city = "San Diego";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
 /// START CONNECTION TO API ///
 
 /// START SEARCH ENGINE ///
